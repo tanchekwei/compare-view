e123e2406963f3ee98db286f8498ad9d4907cbce
tanchekwei <weiwei1@live.com.my>
2021-10-10 17:38:59 +0800

    Support multiple compare view.

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,12 @@
 import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
-	const file1 = vscode.Uri.parse(`untitled:/CompareView1`);
-	const file2 = vscode.Uri.parse(`untitled:/CompareView2`);
+	let count: number = 0;
 	let disposable = vscode.commands.registerCommand('compare-view.compareView', () => {
-		vscode.commands.executeCommand("vscode.diff", file1, file2);
+		vscode.commands.executeCommand(
+			"vscode.diff",
+			vscode.Uri.parse(`untitled:/Compare View ${(count * 2) + 1}`),
+			vscode.Uri.parse(`untitled:/Compare View ${(count * 2) + 2}`));
+		count++;
 	});
 	context.subscriptions.push(disposable);
 }
