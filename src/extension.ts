cfde31df1f50fb3d448ce08031e431df853d47f6
Chek Wei Tan <weiwei1@live.com.my>
2022-10-25 18:03:17 +0800

    Update count to the last compare view tab number on startup, to prevent the extension create compare view start from 1

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,32 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { Tab } from "vscode";
+const name = "Compare View";
+const tabRegex = /\bCompare View \b\d+\b ↔ \b\bCompare View \b\d+/;
 export function activate(context: vscode.ExtensionContext) {
-	let count: number = 0;
-	let disposable = vscode.commands.registerCommand('compare-view.compareView', () => {
-		vscode.commands.executeCommand(
-			"vscode.diff",
-			vscode.Uri.parse(`untitled:/Compare View ${(count * 2) + 1}`),
-			vscode.Uri.parse(`untitled:/Compare View ${(count * 2) + 2}`));
-		count++;
-	});
-	context.subscriptions.push(disposable);
+  let count: number;
+  const labels = vscode.window.tabGroups.all.flatMap(({ tabs }) => tabs)
+    .filter((tab) => {
+      return tabRegex.test(tab.label);
+    })
+    .map((tab) => tab.label);
+  if (labels && labels.length > 0) {
+    let nums: number[] = [];
+    labels.forEach(label => {
+      label.match(/\d+/g)?.forEach(v => { nums.push(Number(v)); });
+    });
+    count = Math.max.apply(null, nums);
+  } else {
+    count = 0;
+  }
+  let disposable = vscode.commands.registerCommand(
+    "compare-view.compareView",
+    () => {
+      vscode.commands.executeCommand(
+        "vscode.diff",
+        vscode.Uri.parse(`untitled:/${name} ${++count}`),
+        vscode.Uri.parse(`untitled:/${name} ${++count}`)
+      );
+    }
+  );
+  context.subscriptions.push(disposable);
 }
